2a1179c5a83b43a3ab8a44f96ab448cf21b104a9
Wndtlr <dkim3@colgate.edu>
2025-11-07 15:23:43 -0500

    Initial setup and Postgresql connection

---
+++ src/main/java/com/trainerteamtoolkit/trainerteamtoolkit/TrainerteamtoolkitApplication.java
@@ -0,0 +1,13 @@
+package com.trainerteamtoolkit.trainerteamtoolkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainerteamtoolkitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainerteamtoolkitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trainerteamtoolkit/trainerteamtoolkit/TrainerteamtoolkitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainerteamtoolkit.trainerteamtoolkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainerteamtoolkitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
